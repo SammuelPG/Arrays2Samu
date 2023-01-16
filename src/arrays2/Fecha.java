92d51fcbd6860bb6aeee395287e1c518bd7c3ffb
SammuelPG <palazongarciasamuel@gmail.com>
2023-01-16 09:46:15 +0100

    ej2

--- src/arrays2/Fecha.java
+++ src/arrays2/Fecha.java
@@ -25,19 +25,24 @@ public class Fecha {
         this.mes = mes;
         this.anno = anno;
     }
-    public boolean comprobarFecha(String fecha){
-    String[] resultado;
-    boolean correcto=true;
-    resultado=fecha.split("/");
-    
-    try{
-     dia= Integer.parseInt(resultado[0]);
-     mes= Integer.parseInt(resultado[1]);
-     anno=Integer.parseInt(resultado[2]);
+    public boolean comprobarFecha(String fecha) {
+        String[] resultado;
+        boolean correcto = true;
+        resultado = fecha.split("/");
+        if (resultado.length != 3) {
+            correcto = false;
+        } else {
+            try {
+                dia = Integer.parseInt(resultado[0]);
+                mes = Integer.parseInt(resultado[1]);
+                anno = Integer.parseInt(resultado[2]);
+                if (mes < 1 || mes > 12) {
+                    correcto = false;
+                }
+            } catch (NumberFormatException ex) {
+                correcto = false;
+            }
+        }
+        return correcto;
     }
-    catch(NumberFormatException ex){
-    correcto=false;
-    }
-    return correcto;
-    }
-    }
+}
